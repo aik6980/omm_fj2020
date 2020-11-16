20ba02e6ab6a67a1b1bbeb9dffdcc126b0dc6d88
aik6980 <aik6980>
2020-11-16 23:54:54 +0000

    cannot check in a file larger than 100 MB so I zip it and provide a batch file (power shell) so we can easily extract it
    1. setup gitignore to ignore Unity temp files in sub folders
    2. check in test_database Unity project, for testing Firebase

---
+++ test_database/Assets/Scripts/SignInHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SignInHandler : MonoBehaviour
+{
+    public TMP_InputField emailIF;
+    public TMP_InputField nicknameIF;
+    public TMP_InputField passwordIF;
+
+    bool signInComplete;
+
+    public void SignUp()
+    {
+        APIHandler.Instance.authAPI.SignupUser(emailIF.text, passwordIF.text, callback: () =>
+        {
+            var user = new User { nickname = nicknameIF.text };
+            APIHandler.Instance.databaseAPI.PostUser(user, 
+                callback: () =>
+                {
+                    APIHandler.Instance.authAPI.CurrentUser = user;
+                    signInComplete = true;
+                }, Debug.Log);
+        }, Debug.Log);
+    }
+
+    public void SignIn()
+    {
+        APIHandler.Instance.authAPI.SigninUser(emailIF.text, passwordIF.text, 
+            callback: () => APIHandler.Instance.databaseAPI.GetUser( user =>
+            {
+                APIHandler.Instance.authAPI.CurrentUser = user;
+                signInComplete = true;
+            }, Debug.Log),
+            Debug.Log);
+    }
+
+    void Update()
+    {
+        if(signInComplete)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
