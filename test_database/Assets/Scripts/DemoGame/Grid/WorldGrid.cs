0ebac2d091790d076d90e185d8cbe7c9e8b187e6
Matt Barth <mbmattbarth@gmail.com>
2020-11-20 14:42:21 -0500

    basic coordinates and healing broken coordinates

---
+++ test_database/Assets/Scripts/DemoGame/Grid/WorldGrid.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGrid : MonoBehaviour
+{
+	public GameObject m_CoordinatePrefab;
+	public List<Coordinate> m_Coordinates = new List<Coordinate>();
+
+	private void Start()
+	{
+		Populate();
+	}
+
+	private void Populate()
+	{
+		int obstacleCount = 0;
+		while (obstacleCount < 10)
+		{
+			int randomX = Random.Range(-10, 10);
+			int randomY = Random.Range(-10, 10);
+
+			// placeholder populate logic...
+			Coordinate coordinate = new BrokenCoordinate(this, new Vector2(randomX, randomY));
+			AddCoordinate(coordinate);
+			obstacleCount++;
+		}
+	}
+
+	private void AddCoordinate(Coordinate c)
+	{
+		var newCoordinate = GameObject.Instantiate<GameObject>(m_CoordinatePrefab, transform);
+		var rep = newCoordinate.GetComponent<CoordinateRepresentation>();
+		rep.Configure(c);
+	}
+
+	public void CoordinateRegistration(Coordinate coordinate, bool registered)
+	{
+		if (registered)
+			m_Coordinates.Add(coordinate);
+		else
+			m_Coordinates.Remove(coordinate);
+	}
+
+}
--- test_database/Assets/Scripts/DemoGame/PlayerCharacter.cs
+++ test_database/Assets/Scripts/DemoGame/PlayerCharacter.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float m_Speed;
+    public CharacterController m_Controller;
 
-    // Update is called once per frame
-    void Update()
+	[HideInInspector]
+	public CoordinateRepresentation m_RelevantCoordinate;
+
+    void FixedUpdate()
     {
-        
-    }
+        Movement();
+		Interactions();
+	}
+
+    private void Movement()
+    {
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		m_Controller.Move(move * Time.deltaTime * m_Speed);
+	}
+	private void Interactions()
+	{
+		if (m_RelevantCoordinate != null && Input.GetKeyDown(KeyCode.Space))
+			m_RelevantCoordinate.PlayerInteracted(this);
+	}
+
+	public void SetRelevantCoordinate(CoordinateRepresentation rep)
+	{
+		if (m_RelevantCoordinate != null && m_RelevantCoordinate != rep)
+			m_RelevantCoordinate.ToggleReaction(false);
+
+		m_RelevantCoordinate = rep;
+		if (m_RelevantCoordinate != null)
+		{
+			Debug.Log("rep reacting");
+			m_RelevantCoordinate.ToggleReaction(true);
+		}
+		else
+		{
+			Debug.Log("rep reacting finished");
+		}
+	}
 }
