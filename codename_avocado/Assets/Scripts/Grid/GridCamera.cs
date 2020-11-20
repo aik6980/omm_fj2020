a628c4cc01d44ff895297a8151497889d9f8322b
Matt Barth <mbmattbarth@gmail.com>
2020-11-20 15:54:11 -0500

    make camera follow player

---
+++ codename_avocado/Assets/Scripts/Grid/GridCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCamera : MonoBehaviour
+{
+    public GridPlayerCharacter m_Player;
+
+    private Vector3 m_PlayerOffset;
+
+    private void Awake()
+    {
+        m_PlayerOffset = m_Player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = m_Player.transform.position - m_PlayerOffset;
+    }
+}
