d1362c48cc917d04dc67e15225d3a8bdaea1cd49
Frictionpowered <denes@nagymathe.com>
2020-11-21 15:12:44 +0000

    added an unfold script and prefab

---
+++ codename_avocado/Assets/Scripts/Util/EventListener.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventListener : MonoBehaviour
+{
+    public delegate void TriggerFunction(Collider col);
+    public delegate void CollisionFunction(Collision coll);
+    public delegate void GameObjectFunction(GameObject g);
+    public delegate void GameObject2Function(GameObject a, GameObject b);
+
+    public event TriggerFunction OnTriggerEnterDelegate = null;
+    public event TriggerFunction OnTriggerExitDelegate = null;
+
+    public event CollisionFunction OnCollisionEnterDelegate = null;
+    public event CollisionFunction OnCollisionExitDelegate = null;
+
+    public event GameObject2Function OnTriggerEnterDelegate2 = null;
+    public event GameObject2Function OnTriggerExitDelegate2 = null;
+
+    public event GameObject2Function OnCollisionEnterDelegate2 = null;
+    public event GameObject2Function OnCollisionExitDelegate2 = null;
+
+    public event GameObjectFunction OnDestroyDelegate = null;
+    public event GameObjectFunction OnEnableDelegate = null;
+    public event GameObjectFunction OnDisableDelegate = null;
+
+    public event GameObjectFunction OnAnimatorIKDelegate = null;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (OnTriggerEnterDelegate != null) OnTriggerEnterDelegate(col);
+        if (OnTriggerEnterDelegate2 != null) OnTriggerEnterDelegate2(this.gameObject, col.gameObject);
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (OnTriggerExitDelegate != null) OnTriggerExitDelegate(col);
+        if (OnTriggerExitDelegate2 != null) OnTriggerExitDelegate2(this.gameObject, col.gameObject);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (OnCollisionEnterDelegate != null) OnCollisionEnterDelegate(collision);
+        if (OnCollisionEnterDelegate2 != null) OnCollisionEnterDelegate2(this.gameObject, collision.gameObject);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (OnCollisionExitDelegate != null) OnCollisionExitDelegate(collision);
+        if (OnCollisionExitDelegate2 != null) OnCollisionExitDelegate2(this.gameObject, collision.gameObject);
+    }
+
+    void OnDestroy()
+    {
+        if (OnDestroyDelegate != null) OnDestroyDelegate(this.gameObject);
+    }
+
+    void OnEnable()
+    {
+        if (OnEnableDelegate != null) OnEnableDelegate(this.gameObject);
+    }
+
+    void OnDisable()
+    {
+        if (OnDisableDelegate != null) OnDisableDelegate(this.gameObject);
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        if (OnAnimatorIKDelegate != null) OnAnimatorIKDelegate(this.gameObject);
+    }
+
+    public static EventListener Get(Collider c)
+    {
+        return Get(c.gameObject);
+    }
+
+    public static EventListener Get(Component c)
+    {
+        return Get(c.gameObject);
+    }
+
+    public static EventListener Get(GameObject go)
+    {
+        EventListener el = go.GetComponent<EventListener>();
+        return el ?? go.AddComponent<EventListener>();
+    }
+
+}
---
+++ codename_avocado/Assets/Scripts/Util/UI_Over_3D.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Over_3D : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 targetOffset;
+    public bool scaleWithDistance = true;
+    public float scaleScale = 25.0f;
+
+    RectTransform myRectTrans;
+    Canvas canvas;
+
+
+    void Start()
+    {
+        myRectTrans = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        if (canvas == null)
+        {
+            canvas = GetComponentInParent<Canvas>();
+        }
+
+        //this is the ui element
+        RectTransform UI_Element;
+
+        //first you need the RectTransform component of your canvas
+        RectTransform CanvasRect = canvas.GetComponent<RectTransform>();
+
+        //then you calculate the position of the UI element
+        //0,0 for the canvas is at the center of the screen, whereas WorldToViewPortPoint treats the lower left corner as 0,0. Because of this, you need to subtract the height / width of the canvas * 0.5 to get the correct position.
+
+        Vector3 worldPos = target.TransformPoint(targetOffset);
+        Vector2 ViewportPosition = Camera.main.WorldToViewportPoint(worldPos);
+        Vector2 WorldObject_ScreenPosition = new Vector2(
+        ((ViewportPosition.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
+        ((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+
+        //now you can set the position of the ui element
+        myRectTrans.anchoredPosition = WorldObject_ScreenPosition;
+
+        if (scaleWithDistance)
+        {
+            float distance = Vector3.Dot(worldPos - Camera.main.transform.position, Camera.main.transform.forward);
+            myRectTrans.localScale = Vector3.one * (scaleScale / distance);
+        }
+    }
+
+}
