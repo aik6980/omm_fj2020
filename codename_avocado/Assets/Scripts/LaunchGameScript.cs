678a90874ac29536422e53d673ac877a716fe633
Tim-maC <51458034+Tim-maC@users.noreply.github.com>
2020-11-22 19:17:15 +0000

    start menu + PH background + menu scripts

---
+++ codename_avocado/Assets/Scripts/LaunchGameScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaunchGameScript : MonoBehaviour
+{
+    public void LaunchGameScene1() {
+        SceneManager.LoadScene("BandaidGameScene");
+    }
+
+}
+
+
+
+
+
+
+
