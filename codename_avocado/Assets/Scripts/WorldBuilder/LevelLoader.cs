41004ed7c446d182f5aed9a4b6695f9192514723
Guantes188 <guantes188@hotmail.com>
2020-11-22 13:38:53 +0000

    Adding missed files.

---
+++ codename_avocado/Assets/Scripts/WorldBuilder/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILevelLoader
+{
+    WorldGrid.WorldData LoadLevel(WorldGrid grid);
+}
+
+public class LevelLoader : MonoBehaviour, ILevelLoader
+{
+    public int Level = 1;
+
+    public WorldGrid.WorldData LoadLevel(WorldGrid grid)
+    {
+        return WorldBuilder.GetOrCreateInstance().BuildLevel(grid, this.Level++);
+    }
+}
---
+++ codename_avocado/Assets/Scripts/WorldBuilder/TestLevelLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLevelLoader : MonoBehaviour, ILevelLoader
+{
+    public int m_Distance = 20;
+
+    public WorldGrid.WorldData LoadLevel(WorldGrid grid)
+    {
+        return WorldBuilder.GetOrCreateInstance().BuildDefaultTest(grid, m_Distance, 1, 3, 3);
+    }
+}
