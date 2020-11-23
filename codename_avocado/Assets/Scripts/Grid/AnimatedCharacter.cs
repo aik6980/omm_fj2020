7697ca22cf46323ed6c0cf3dc2f50cfc286c8d85
Frictionpowered <denes@nagymathe.com>
2020-11-23 19:37:36 +0000

    new animated prefab!

--- codename_avocado/Assets/Scripts/Grid/AnimatedCharacter.cs
+++ codename_avocado/Assets/Scripts/Grid/AnimatedCharacter.cs
@@ -6,7 +6,10 @@ public class AnimatedCharacter : MonoBehaviour
 {
     public GameObject player;       // follow this
     public GridPlayerCharacter gridPC;
+    //to be removed
     public Animation animation;     // set the animations in this
+    //new
+    public Animator animtr;
 
     public Unfold unfold;           // should it be here?
 
@@ -39,11 +42,27 @@ public class AnimatedCharacter : MonoBehaviour
             gridPC.OnSpawnDelegate.AddListener(OnSpawn);
         }
 
-        Spawn();
+        //Spawn();
+        if (animtr)
+            animtr.gameObject.SetActive(true);
+
+    }
+
+    public void SetPC(GridPlayerCharacter gpc)
+    {
+        gridPC = gpc;
+        player = gpc.gameObject;
+        if (gridPC)
+        {
+            Debug.Log("setpc");
+            gridPC.OnPlaceDelegate += OnPlace;
+            gridPC.OnSpawnDelegate.AddListener(OnSpawn);
+        }
     }
 
     void OnSpawn()
     {
+        Debug.Log("onspawn", this);
         if (!jumping)
             Spawn();
     }
@@ -53,15 +72,42 @@ public class AnimatedCharacter : MonoBehaviour
         Debug.Log("jump", this);
         moving = false;
         jumping = true;
-        animation.Play(animClips[2]);
+        if (animation)
+        {
+            animation.Play(animClips[2]);
+        }
+        if (animtr)
+            animtr.SetBool("jumping", true);
     }
 
     void Spawn()
     {
+        Debug.Log("spawn?", this);
+        if (player == null) return;
+        if (gridPC == null) return;
+
+        Debug.Log("spawn", this);
+
         this.transform.rotation = player.transform.rotation;
         this.transform.position = player.transform.position;
-        animation.gameObject.SetActive(true);
-        animation.Play(animClips[0]);
+
+        if (animation)
+        {
+            //show model
+            animation.gameObject.SetActive(true);
+            animation.Play(animClips[0]);
+        }
+        if (animtr)
+        {
+            //show model
+            animtr.gameObject.SetActive(true);
+
+            animtr.SetBool("moving", false);
+            animtr.SetBool("jumping", false);
+            animtr.SetBool("unfolding", false);
+            animtr.SetBool("dead", false);
+            animtr.SetTrigger("Reset");
+        }
 
         //pick shape
         //ToDo: based on unfold shape
@@ -107,6 +153,8 @@ public class AnimatedCharacter : MonoBehaviour
 
     void Update()
     {
+        if (player == null) return;
+
         //hack
         //animation.transform.localPosition = Vector3.down * 0.8f;
 
@@ -119,24 +167,51 @@ public class AnimatedCharacter : MonoBehaviour
             //2. activate Unfold and hide the character mesh
             //3. play the procedural unfolding from 0 to 100% over some time
             //4. THEN ready to respawn
-            if (animation.IsPlaying(animClips[2]))
-            { //wait
-                //Debug.Log("jumping...");
-                return;
-            }
-            else
+            if (animation)
             {
-                Debug.Log("jump done.");
-                jumping = false;
+                if (animation.IsPlaying(animClips[2]))
+                { //wait
+                  //Debug.Log("jumping...");
+                    return;
+                }
+                else
+                {
+                    Debug.Log("jump done.");
+                    jumping = false;
 
-                unfolding = true;
-                unfold.progress = 0;
-                unfold.SpawnFaces();
-                animation.gameObject.SetActive(false);
-                return;
-                //Spawn();
+                    unfolding = true;
+                    unfold.progress = 0;
+                    unfold.SpawnFaces();
+                    //hide model
+                    animation.gameObject.SetActive(false);
+                    return;
+                    //Spawn();
+                }
+            }
+            if (animtr)
+            {
+                animtr.SetBool("jumping", true);
+                AnimatorStateInfo asi = animtr.GetCurrentAnimatorStateInfo(0);
+                if (asi.normalizedTime < 0.99f)
+                {//wait
+                    //ToDo: make sure it reaches the target transform by the time the anim ends
+                    float timeLeft = asi.length * 1.0f - asi.normalizedTime;
+                    Debug.Log(timeLeft);
+                } else
+                {//finished
+                    Debug.Log("jump done.");
+                    jumping = false;
+
+                    unfolding = true;
+                    unfold.progress = 0;
+                    unfold.SpawnFaces();
+                    //hide model
+                    animtr.gameObject.SetActive(false);
+                    return;
+                }
             }
         }
+
         if (unfolding)
         {
             if (unfold.Finished())
@@ -155,17 +230,26 @@ public class AnimatedCharacter : MonoBehaviour
         offset = player.transform.position - this.transform.position;
         //offset.z = ((player.transform.rotation.y - this.transform.rotation.y) + 180.0f) % 360.0f - 180.0f;
 
-        if (!moving && offset.magnitude > 0.1f)
+        if (animation)
         {
-            moving = true;
-            animation.Play(animClips[1]);
-        } else
-        if (moving && offset.magnitude <= 0.1f)
-        {
-            moving = false;
-            animation.Play(animClips[0]);
+            if (!moving && offset.magnitude > 0.1f)
+            {
+                moving = true;
+                animation.Play(animClips[1]);
+            }
+            else
+            if (moving && offset.magnitude <= 0.1f)
+            {
+                moving = false;
+                animation.Play(animClips[0]);
+            }
         }
 
+        if (animtr)
+        {
+            moving = offset.magnitude > 0.1f;
+            animtr.SetBool("moving", moving);
+        }
 
         Quaternion faceDir = moving ? Quaternion.LookRotation(offset) : player.transform.rotation;
         this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, faceDir, 400.0f * dT);
