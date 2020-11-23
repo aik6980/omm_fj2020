11f66e3b198e8456d4c12c7b928299fd5ba0ac73
Tim-maC <51458034+Tim-maC@users.noreply.github.com>
2020-11-23 01:35:42 +0000

    Initial in-game menu, disabled for now, still needs some fixing

---
+++ codename_avocado/Assets/Scripts/EscapeKeyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// public class EscapeKeyScript : MonoBehaviour
+// {
+//     public OptionsMenu optionsMenu;
+
+//     void Start () {
+
+//     }
+
+//     void Update () {
+
+//         if(Input.GetKeyDown(KeyCode.Escape))
+//         {
+//             optionsMenu.gameObject.SetActive(!optionsMenu.gameObject.activeSelf);
+
+//         }
+
+//     }
+
+// }
+
+
+
+
+
+
+
