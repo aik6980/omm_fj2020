4423013c85f937ecc9ec3dc8c49288aefeda6889
Frictionpowered <denes@nagymathe.com>
2020-11-25 23:43:26 +0000

    materials

---
+++ codename_avocado/Assets/Scripts/SelfDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    public float timer;
+    public float randomRange = 0;
+
+    void Start()
+    {
+        Destroy(this.gameObject, timer + randomRange * (Random.value*2.0f -1.0f));
+    }
+
+}
