b94ff75c917860e69e1ca914ecbeffc936d55f89
Frictionpowered <denes@nagymathe.com>
2020-11-26 17:47:57 +0000

    pause menu v0.0001

---
+++ codename_avocado/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject root;
+
+
+    void Start()
+    {
+        root.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!root.activeSelf)
+        {//inactive
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                root.SetActive(true);
+            }
+        } else
+        {//active
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 1;
+                root.SetActive(false);
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        root.SetActive(false);
+    }
+
+    public void QuitToMenu()
+    {
+        Application.LoadLevel(0);
+    }
+}
