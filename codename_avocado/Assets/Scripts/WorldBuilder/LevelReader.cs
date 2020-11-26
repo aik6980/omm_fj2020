fe279e645e0db0c6e31db3951128c4f4d7936ba0
Guantes188 <guantes188@hotmail.com>
2020-11-26 00:52:23 +0000

    Added support for tab separated csv's

--- codename_avocado/Assets/Scripts/WorldBuilder/LevelReader.cs
+++ codename_avocado/Assets/Scripts/WorldBuilder/LevelReader.cs
@@ -64,7 +64,7 @@ public class LevelReader : MonoSingleton<LevelReader>
 
 		for (int y = 0; y < line.Length; ++y)
 		{
-			var part = line[y].Split(',');
+			var part = line[y].Split(',', '\t');
 			width = Mathf.Max(width, part.Length);
 			for (int x = 0; x < part.Length; ++x)
 			{
