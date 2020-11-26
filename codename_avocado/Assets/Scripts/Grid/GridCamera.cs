093e20bd28b74754cc2583fa530ce6f83243e5bb
Frictionpowered <denes@nagymathe.com>
2020-11-26 22:27:34 +0000

    ChillCam(TM) :)

--- codename_avocado/Assets/Scripts/Grid/GridCamera.cs
+++ codename_avocado/Assets/Scripts/Grid/GridCamera.cs
@@ -7,6 +7,7 @@ public class GridCamera : MonoBehaviour
     public Transform m_Player;
 
     private Vector3 m_PlayerOffset;
+    public float m_strength = 1.0f;
 
     private void Awake()
     {
@@ -16,6 +17,7 @@ public class GridCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = m_Player.position - m_PlayerOffset;
+        //transform.position = m_Player.position - m_PlayerOffset;
+        transform.position = Vector3.Lerp(transform.position, m_Player.position - m_PlayerOffset, Time.deltaTime * m_strength);
     }
 }
