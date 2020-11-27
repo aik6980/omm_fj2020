d5cbfa961ef7f9f80c0d78593f69255fd7693d9b
Guantes188 <guantes188@hotmail.com>
2020-11-27 12:52:39 +0000

    Colour matched the Unity grey.

--- codename_avocado/Assets/Fader.cs
+++ codename_avocado/Assets/Fader.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Fader : MonoBehaviour
 {
     public Graphic graphic;
+    public Color fade_from_colour = Color.black;
 
     void Start()
     {
@@ -15,7 +16,7 @@ public class Fader : MonoBehaviour
             while (a < 1.0f)
             {
                 a = Mathf.Clamp(a + (Time.unscaledDeltaTime * 1f), 0f, 1f);
-                graphic.color = Color.Lerp(Color.black, Color.white, a);
+                graphic.color = Color.Lerp(fade_from_colour, Color.white, a);
                 yield return null;
             }
         }
